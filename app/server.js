137cc5d46947ad4c2bbe295b945422430758f6e9
robertomuggianu <l.sotgia.1382@vallauri.edu>
2023-04-17 08:32:48 +0200

    Primo commit del progetto

---
+++ app/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express=require("express");
+const HTTPS=require("https");
+const fs=require("fs");
+const app=express();
+
+const HTTPSCredentials=require('./config/HTTPSCredentials');
+
+const port=process.env.PORT || 8080;
+const address=process.env.ADDRESS || "127.0.0.1";
+
+let httpsServer=HTTPS.createServer(HTTPSCredentials.Get(),app);
+httpsServer.listen(port,address,function(){
+    console.log("[SERVER] online on port: %s...",port);
+})
