0603fe84d928d014e81f16ac5351b953d8218a77
unknown <l.sotgia.1382@vallauri.net>
2023-05-02 11:38:54 +0200

    Continuazione parte di login e modifiche alla navbar

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -12,9 +12,25 @@ $(()=>{
         $("#sectionRegister").hide();
     });
 
-    // $("btnInviaLogin").on("click", function(){
-    //     let _email = $("#sectionLogin").children('input[name="email"').val();
-    //     console.log(_email);
-    // });
-
+    $("#btnInviaLogin").on("click", function(){
+        let _email = $("#sectionLogin").find("input[name='email']").val();
+        let _password = $("#sectionLogin").find("input[name='pass']").val();
+    
+        if(_email === "" || _password === "" || !ValidateEmail(_email))
+            return null;
+        else
+            console.log("Campi ok");
+            // richiesta login
+    });
+    $("#btnInviaRegister").on("click", function(){
+        let _email = $("#sectionRegister").find("input[name='email']").val();
+        let _nome = $("#sectionRegister").find("input[name='nome']").val();
+        let _cognome = $("#sectionRegister").find("input[name='cognome']").val();
+    
+        if(_email === "" || _nome === "" || _cognome === "" || !ValidateEmail(_email))
+            return null;
+        else
+            console.log("Campi ok");
+            // richiesta login
+    });
 });
---
+++ app/public/js/utils.js
@@ -0,0 +1,8 @@
+/*UTILS.JS*/
+
+function ValidateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        return (true)
+    }
+    return (false)
+}
